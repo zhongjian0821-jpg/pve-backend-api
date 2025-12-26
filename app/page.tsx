2b7fb09d19497c0bec2807a926fefaae715ecf21
zhongjian0821-jpg <zhongjian0821@gmail.com>
2025-12-26 22:54:29 +0800

    Add app/page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div>
+      <h1>PVE Operations Center API</h1>
+      <p>API is running. Visit /api/admin/auth/login to test.</p>
+    </div>
+  )
+}
